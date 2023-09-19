1546a69d3f576bdbdc7c21bcb2201cb1a85aa2e0
Markus Demarmels <markus.demarmels@bluewin.ch>
2023-09-19 23:46:11 +0200

    Ich proiere es wieder...

---
+++ client/script.js
@@ -0,0 +1,138 @@
+if (location.host.includes("localhost")) {
+  // Load livereload script if we are on localhost
+  document.write(
+    '<script src="http://' +
+      (location.host || "localhost").split(":")[0] +
+      ':35729/livereload.js?snipver=1"></' +
+      "script>"
+  );
+
+  function guidGenerator() {
+    var S4 = function () {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (
+      S4() +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      S4() +
+      S4()
+    );
+  }  
+}
+
+const backendUrl = window.location.origin
+  .replace(/^http/, "ws")
+  .replace(/^https/, "wss");
+const socket = new WebSocket(backendUrl);
+
+const userId = guidGenerator();
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!! DON'T TOUCH ANYTHING ABOVE THIS LINE !!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const users = [];
+
+socket.addEventListener("open", async (event) => {
+  console.log("WebSocket connected!");
+  // TODO: create message object to transmit the user to the backend
+});
+
+socket.addEventListener("message", (event) => {
+  const messageObject = JSON.parse(event.data);
+  console.log("Received message from server: " + messageObject.type);
+  console.log("Received message data from server: " + messageObject.data);
+  switch (messageObject.type) {
+    case "ping":
+      socket.send(JSON.stringify({ type: "pong", data: "FROM CLIENT" }));
+      break;
+    case "users":
+      console.log("Here come the userNames...");
+      showUsers(messageObject.data);
+      break;
+    case "message":
+      // TODO: Show new message as DOM element append to chat history
+      break;
+    default:
+      console.error("Unknown message type: " + messageObject.type);
+  }
+});
+
+function showUsers(users) {
+  // TODO: Show the current users as DOM elements
+  if(users!= null){
+    
+    let sendUsers = JSON.parse(users);
+
+    console.log("sendUsers =" + sendUsers);
+    
+    let usersString = "";
+
+    sendUsers.forEach((userName, index) =>  {
+
+      console.log("der nächste Username ist: " + userName);
+
+      usersString += "<div>" + userName + "<div>";
+   
+    })
+
+    console.log("usersString =" + usersString);
+
+    let anchor = document.getElementById("anchor-contact-list");
+    anchor.innerHTML  = usersString;
+  
+  }
+}
+
+function showMessage(message) {
+  // TODO: Show new message as DOM element append to chat history
+}
+
+socket.addEventListener("close", (event) => {
+  console.log("WebSocket closed.");
+});
+
+socket.addEventListener("error", (event) => {
+  console.error("WebSocket error:", event);
+});
+
+function changeUserName() {
+  // TODO: Implement change userName and forward new userName to backend
+
+  const newUserName =  document.getElementById('new-user-name').value;
+  const messageObject  = {
+    type: 'user', 
+    data: {userName: newUserName,
+           userId: userId}
+  };
+  console.log("The userName has changed:" + JSON.stringify(messageObject));  
+  socket.send(JSON.stringify(messageObject));
+
+  const newHeaderTitle =  document.getElementById('chatters-name');
+  console.log(newUserName + ", Dein Chat ist hier!");
+  newHeaderTitle.innerText = newUserName + ", Dein Chat ist hier!";
+  newUserName.innerText = "";
+
+}
+
+function sendMessage() {
+
+  const messageValue =  document.getElementById('written-message').value;
+  const messageObject  = {
+    type: 'message', 
+    data: {message: messageValue,
+           userId: userId}
+  };
+  
+  socket.send(JSON.stringify(messageObject));
+
+}
---
+++ server/backend.js
@@ -0,0 +1,107 @@
+const WebSocket = require("ws");
+const redis = require("redis");
+let redisClient;
+
+let clients = [];
+let messageHistory = [];
+const users = new Map();
+
+// Initiate the websocket server
+const initializeWebsocketServer = async (server) => {
+  redisClient = redis.createClient({
+    socket: {
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+    },
+  });
+  await redisClient.connect();
+
+  const websocketServer = new WebSocket.Server({ server });
+  websocketServer.on("connection", onConnection);
+  websocketServer.on("error", console.error);
+};
+
+// If a new connection is established, the onConnection function is called
+const onConnection = (ws) => {
+  console.log("New websocket connection");
+  ws.on("close", () => onClose(ws));
+  ws.on("message", (message) => onClientMessage(ws, message));
+  // TODO: Send all connected users and current message history to the new client
+  ws.send(JSON.stringify({ type: "ping", data: "FROM SERVER" }));
+  clients.push(ws);
+};
+
+// If a new message is received, the onClientMessage function is called
+const onClientMessage = async (ws, message) => {
+  const messageObject = JSON.parse(message);
+  console.log("I have received a message from client with type: " + messageObject.type);
+  switch (messageObject.type) {
+    case "pong":
+      console.log("Received from client: " + messageObject.data);
+      break;
+    case "user":
+      // TODO: Publish all connected users to all connected clients
+      console.log("new userName entered: " + message);
+      publishNewUser(messageObject);
+      break;
+    case "message":
+      console.log("The message was: " + messageObject.data);
+      renderMessage(messageObject);
+      break;
+    default:
+      console.error("Unknown message type: " + messageObject.type);
+  }
+};
+
+// If a connection is closed, the onClose function is called
+const onClose = async (ws) => {
+  console.log("Websocket connection closed");
+  // TODO: Remove related user from connected users and propagate new list
+};
+
+const getMessageHistory = async () => {
+  return await redisClient.get("messageHistory");
+};
+
+const setMessageHistory = async (messageHistory) => {
+  await redisClient.set("messageHistory", messageHistory);
+};
+
+
+const renderMessage = async (messageObject) => {
+  // TODO: Publish new message to all connected clients and save in redis
+  console.log("Here we are!");
+
+  /* Save the message in the message history. */
+  messageHistory.push(messageObject);
+  await setMessageHistory(JSON.stringify(messageHistory));
+
+  /* Redistribute the message. */
+  for (const client of clients) {
+    client.send(JSON.stringify(messageObject));
+  }
+}
+
+
+function publishNewUser(messageObject){
+
+  console.log("messageObject.data.userName = " + messageObject.data.userName);
+
+  if(messageObject.data.userName != undefined){
+
+    users.set(messageObject.data.userId, messageObject.data.userName); 
+    console.log("meine users sind (als Map):" + JSON.stringify(Object.fromEntries(users)));
+    let userNamesArray = Array.from(users.values());
+    console.log("meine users sind:" + JSON.stringify(userNamesArray));
+
+    let newUsersObject = {
+      type: "users",
+      data:  JSON.stringify(userNamesArray)}
+
+    for (let client of clients) {
+      client.send(JSON.stringify(newUsersObject));
+    }  
+  }
+}
+
+module.exports = { initializeWebsocketServer };
